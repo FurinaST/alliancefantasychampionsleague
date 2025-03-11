186b913446ee25d5ccbc02bd8b1244d3ad865427
FurinaST <ncstrunk@gmail.com>
2025-03-10 18:30:58 -0600

    Add files via upload

---
+++ src/lib/utils/tabs.js
@@ -0,0 +1,61 @@
+import {leagueID} from '$lib/utils/leagueInfo';
+
+export const tabs = [
+    {
+        icon: 'home',
+        label: 'Home',
+        dest: '/',
+    },
+    {
+        icon: 'sports',
+        label: 'Matchups',
+        dest: '/matchups',
+    },
+    {
+        icon: 'swap_horiz',
+        label: 'Trades & Waivers',
+        dest: '/transactions',
+    },
+    {
+        icon: 'view_comfy',
+        label: 'League Info',
+        nest: true,
+        children: [
+            {
+                icon: 'storage',
+                label: 'Rosters',
+                dest: '/rosters',
+            },
+            {
+                icon: 'local_fire_department',
+                label: 'Rivalry',
+                dest: '/rivalry',
+            },
+            {
+                icon: 'leaderboard',
+                label: 'Standings',
+                dest: '/standings',
+            },
+            {
+                icon: 'view_comfy',
+                label: 'Drafts',
+                dest: '/drafts',
+            },
+            {
+                icon: 'military_tech',
+                label: 'Records',
+                dest: '/records',
+            },
+            {
+                icon: 'sports_football',
+                label: 'Go to Sleeper',
+                dest: `https://sleeper.app/leagues/${leagueID}`,
+            },
+        ]
+    },
+    {
+        icon: 'lightbulb',
+        label: 'Resources',
+        dest: '/resources',
+    },
+];
